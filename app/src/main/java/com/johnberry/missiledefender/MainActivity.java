ffbf09e7a5e2c5530ffb91b5b99e83fba4fad039
Nighthawk <jb@Nighthawks-MBP.attlocal.net>
2021-03-10 18:20:55 -0600

    Fullscreen fixed; Animated cloud up & down; Need continuous scroll

--- app/src/main/java/com/johnberry/missiledefender/MainActivity.java
+++ app/src/main/java/com/johnberry/missiledefender/MainActivity.java
@@ -2,52 +2,62 @@ package com.johnberry.missiledefender;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private boolean fullScreen = true;
+    public static int screenHeight;
+    public static int screenWidth;
+    private static final float yCoord = 50;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        requestWindowFeature(Window.FEATURE_NO_TITLE);
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setupFullScreen();
+        getScreenDimensions();
+
+        ImageView cloudImage = findViewById(R.id.cloudImage);
+        ViewGroup layout = findViewById(R.id.layout);
+
+        new ScrollingBackground(this,
+                layout, R.drawable.clouds, 4000);
+
+        cloudImage.setY(yCoord);
+        ObjectAnimator oa =
+                ObjectAnimator.ofFloat(cloudImage, "y", yCoord + 175);
+        oa.setStartDelay(500);
+        oa.setDuration(2000);
+        oa.setRepeatMode(ValueAnimator.REVERSE);
+        oa.setRepeatCount(ValueAnimator.INFINITE);
+        oa.start();
     }
 
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if (hasFocus) {
-            if (fullScreen) {
-                hideSystemUI();
-            } else {
-                showSystemUI();
-            }
-        }
+    private void getScreenDimensions() {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        screenHeight = displayMetrics.heightPixels;
+        screenWidth = displayMetrics.widthPixels;
     }
 
-    private void hideSystemUI() {
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+    private void setupFullScreen() {
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
-    }
-
-    private void showSystemUI() {
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 }
